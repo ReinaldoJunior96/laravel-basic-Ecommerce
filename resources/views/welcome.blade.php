39d10725b1ac50e07f56f38ff29c6bfe819b1f28
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2021-02-26 16:59:12 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<h1 class="porra">reinaldo teste</h1>
+
+
+</body>
+</html>
